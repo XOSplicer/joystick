3a60e47134b77d3e376a014cc1d57f8ef074062c
Felix Stegmaier <stegmaier.felix@gmail.com>
2017-01-18 22:23:17 +0100

    working but laggy

--- src/main.rs
+++ src/main.rs
@@ -2,54 +2,48 @@ extern crate glfw;
 
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::thread::JoinHandle;
+use std::time;
 use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
 
 use glfw::{Joystick, JoystickId, Action};
 
 fn main() {
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).expect("Could not load GLFW!");
-    let joystick = JoystickHandle(glfw.get_joystick(JoystickId::Joystick1));
-    let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
-        let mut running = true;
-        let mut exit_pressed = false;
-        while running {
-            let data = joystick.get_data();
-            match data {
-                Ok(t) => {
-                    match t.buttons[9] {
-                        Action::Press => exit_pressed = true,
-                        Action::Release => {
-                            if exit_pressed {
-                                running = false;
-                            }
-                        },
-                        _ => panic!("dont hold the button")
-                    }
-                    tx.send(Message::Data(t)).unwrap();
-                },
-                Err(e) => {
-                    println!("Error: {}", e);
-                    running = false;
+    let glfw = glfw::init(glfw::FAIL_ON_ERRORS).expect("Could not load GLFW!");
+    let joystick = JoystickHandle::new(glfw.get_joystick(JoystickId::Joystick1));
+    let joystick_thread = joystick.spin_up();
+    loop {
+        let data = joystick_thread.rx.recv().unwrap();
+        match data {
+            Ok(d) => {
+                println!("{:?}", &d);
+                if d.buttons[0] == Action::Press {
+                    println!("Pressed Button 0, exit!");
+                    joystick_thread.tear_down().unwrap();
+                    break;
                 }
+            },
+            Err(e) => {
+                println!("Error: {}", e);
+                joystick_thread.tear_down().unwrap();
+                break;
             }
-            std::thread::sleep(std::time::Duration::from_millis(10));
         }
-        tx.send(Message::Exit)
-    });
-    while let Message::Data(t) = rx.recv().unwrap() {
-        println!("{:?}", t);
+        thread::sleep(time::Duration::from_millis(10));
     }
-
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Message<T> {
-    Exit,
-    Data(T)
+#[derive(Clone, Copy)]
+struct JoystickHandle {
+    inner: Joystick,
 }
 
-struct JoystickHandle(Joystick);
+struct JoystickThreadHandle {
+    thread: JoinHandle<bool>,
+    rx: Receiver<Result<JoystickData, String>>,
+    running: Arc<Mutex<bool>>
+}
 
 #[derive(Debug, Clone)]
 struct JoystickData {
@@ -59,12 +53,19 @@ struct JoystickData {
 }
 
 impl JoystickHandle {
+
+    fn new(handle: Joystick) -> Self {
+        JoystickHandle {
+            inner: handle,
+        }
+    }
+
     fn get_data(&self) -> Result<JoystickData, String> {
-        match self.0.is_present() {
+        match self.inner.is_present() {
             true => Ok(JoystickData {
-                    name: self.0.get_name(),
-                    axes: self.0.get_axes(),
-                    buttons: self.0.get_buttons()
+                    name: self.inner.get_name(),
+                    axes: self.inner.get_axes(),
+                    buttons: self.inner.get_buttons()
                                     .iter().map(|&x| match x {
                                         0 => Action::Release,
                                         1 => Action::Press,
@@ -75,4 +76,30 @@ impl JoystickHandle {
             false => Err("Joystick not present".to_owned())
         }
     }
+
+    fn spin_up(self) -> JoystickThreadHandle {
+        let (tx, rx) = mpsc::channel();
+        let running_orig = Arc::new(Mutex::new(true));
+        let running = running_orig.clone();
+        let joinhandle = thread::spawn(move || {
+            while *running.lock().unwrap() {
+                tx.send(self.get_data()).unwrap();
+                thread::sleep(time::Duration::from_millis(5));
+            }
+            false
+        });
+        JoystickThreadHandle {
+            thread: joinhandle,
+            rx: rx,
+            running: running_orig
+        }
+    }
+}
+
+impl JoystickThreadHandle {
+    fn tear_down(self) -> std::result::Result<bool, Box<std::any::Any + Send + 'static>> {
+        *self.running.lock().unwrap() = false;
+        self.thread.join()
+    }
+
 }
